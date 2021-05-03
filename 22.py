2ca8ca6757b7864d3a1fd6a0b4b41a41b74e7710
marylinfernandez <20180259@lamolina.edu.pe>
2021-05-03 04:23:29 -0500

    agregando archivo py a la rama web

---
+++ 22.py
@@ -0,0 +1,60 @@
+"""from math import pi
+radio = float(input('Ingrese el radio del círculo: '))
+#Menú de opciones se muestra a continuación
+print("Escoga la letra que corresponde a la opción que desea:")
+print("a. Calcular el díámetro")
+print("b. Calcular el perímetro")
+print("c. Calcular el área")
+eleccion = input('Ingresa la letra de la opción elegida: ')
+if eleccion == "a":
+    #Calculo del diámetro
+    diametro = 2*radio
+print('el diámetro del círculo es:',diametro)
+"""
+
+"""
+for i in range (0,5):
+    for j in range (0,3):
+        print (i,j)
+"""
+
+"""
+#programa para primos y no primos 
+numero = int(input("ingrese un número entero: "))
+if numero > 1:
+    primo = True
+    i = 2
+    while i < numero:
+        if numero % i == 0:
+             primo = False
+             break
+        i=i+1
+print(primo)        
+        
+"""     
+"""
+for i in range(-10,7,4):
+    print(i)
+"""
+"""
+for i in range (0,5):
+    for j in range (0,3):
+        print (i*j)
+"""
+
+while 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
